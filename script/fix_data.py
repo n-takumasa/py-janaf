9b593e830e8763f0d1b0d6880d3e50e87260c48e
Takumasa Nakamura <n.takumasa@gmail.com>
2024-01-11 09:56:45 +0900

    Revert "enh: fix_data: fix_space_to_tab"
    This reverts commit a64c5ffb53a789d4f8adb52bc64d871bbe8c5614.

--- script/fix_data.py
+++ script/fix_data.py
@@ -7,29 +7,15 @@ from pathlib import Path
 def fix(fp: Path) -> bool:
     with fp.open("r+") as f:
         orig = f.read()
-        result = orig
-        result = fix_missing_tab(result)
-        result = fix_space_to_tab(result)
-    if orig == result:
-        return False
-    with fp.open("w") as f:
-        f.write(result)
+        text = orig.split("\n", 1)[1]
+        # eg. C-083.txt
+        # 288.5001548.088	111.062	118.520	-2.152	II <--> III
+        #       ^^ : missing tab
+        if re.search(r"\.\d+\.", text) is None:
+            return False
+        f.seek(0)
+        f.write(re.sub(r"(\.\d{3})(\d)", r"\1\t\2", orig))
         return True
-
-
-def fix_missing_tab(text: str) -> str:
-    # eg. C-083.txt
-    # 288.5001548.088	111.062	118.520	-2.152	II <--> III
-    #       ^^ : missing tab
-    if re.search(r"\.\d+\.", text) is None:
-        return text
-    return re.sub(r"(\.\d{3})(\d)", r"\1\t\2", text)
-
-
-def fix_space_to_tab(text: str) -> str:
-    if re.search(r"\d +\d", text) is None:
-        return text
-    return re.sub(r"(\d) +(\d)", r"\1\t\2", text)
 
 
 def main():
