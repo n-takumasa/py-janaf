55503332c40e9720078d2e9af9766e490adfdb4e
Takumasa Nakamura <n.takumasa@gmail.com>
2024-06-12 16:33:01 +0900

    mnt: migirate to hatch

--- noxfile.py
+++
@@ -1,8 +0,0 @@
-from nox_poetry import Session, session
-
-
-@session(python=["3.9", "3.10", "3.11", "3.12"])
-def tests(session: Session):
-    session.install(".")
-    session.install("pytest")
-    session.run("pytest")
