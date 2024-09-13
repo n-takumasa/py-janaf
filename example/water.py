79e4371ddbaf0a82a520b3e0dd5e13b632bfdd1b
Takumasa Nakamura <n.takumasa@gmail.com>
2024-09-13 15:54:28 +0900

    tst: fixup

--- example/water.py
+++ example/water.py
@@ -3,9 +3,13 @@
 # dependencies = [
 #   "janaf",
 #   "matplotlib",
+#   "pandas >= 1.5.0",  # polars.DataFrame.to_pandas
+#   "pyarrow >= 8.0.0",  # polars.DataFrame.to_pandas
 # ]
 # ///
 
+import os
+from matplotlib.axes import Axes
 import matplotlib.pyplot as plt
 import polars as pl
 
@@ -16,8 +20,8 @@ t = janaf.search(name="Water, 1 Bar")
 df = t.df.to_pandas(use_pyarrow_extension_array=True).set_index("T(K)")
 
 fig, axes = plt.subplots(4, 2, sharex="col")
+ax: Axes
 for ax, col in zip(axes.flat, [c for c in df.columns if c not in {"Note"}]):
-    ax: plt.Axes
     df.plot(y=col, ax=ax, legend=False)
     ax.set_title(col)
     if col in {"Cp", "S", "-[G-H(Tr)]/T"}:
@@ -29,10 +33,10 @@ xs = (
     t.df.drop_nulls("Note").select(pl.col("T(K)").unique()).get_column("T(K)").to_list()
 )
 for ax in axes.flat:
-    ax: plt.Axes
     for x in xs:
         ax.axvline(x, color="k", linestyle=":")
 
 fig.suptitle(f"{t.name} {t.formula}")
 fig.tight_layout()
-plt.show()
+if not os.getenv("CI"):
+    plt.show()
---
+++ tests/test_example.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+import subprocess
+import sys
+import pytest
+
+
+def get_example_scripts():
+    return [
+        pytest.param(fp, id=fp.name)
+        for fp in Path(__file__).parents[1].glob("example/**/*.py")
+    ]
+
+
+@pytest.mark.parametrize(
+    "fp",
+    get_example_scripts(),
+)
+def test_example(fp: Path):
+    env = os.environ.copy()
+    env["CI"] = "true"
+    ret = subprocess.run(
+        [sys.executable, fp.absolute()],
+        env=env,
+    )
+    assert ret.returncode == 0
